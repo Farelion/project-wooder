745d791543f777219d4388319236d9834a34cff9
Farelion <mczownik@gmail.com>
2019-08-13 20:44:50 +0200

    mobie-menu-locked

---
+++ js/mobile-menu.js
@@ -0,0 +1,27 @@
+  
+  const showDialog = () => {
+    if ($(".nav-mobile").hasClass("on")){
+      const body = document.body;
+      const scrollY = body.style.top;
+      body.style.position = '';
+      body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+      $('.menu-toggle').toggleClass("on");
+      $('.nav-mobile').toggleClass("on");
+      $("nav ul").toggleClass('hidden');
+      $("nav div").toggleClass('hidden');
+    }else{
+      $('.menu-toggle').toggleClass("on");
+      $('.nav-mobile').toggleClass("on");
+      $("nav ul").toggleClass('hidden');
+      $("nav div").toggleClass('hidden');
+      const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+      const body = document.body;
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollY}`;
+    }
+  };
+
+  window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+  });
--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(".menu-toggle").on('click', function() {
-    $(this).toggleClass("on");
-    $('.nav-mobile').toggleClass("on");
-    $("nav ul").toggleClass('hidden');
-    $("nav div").toggleClass('hidden');
-  });
