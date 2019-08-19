3dd525a31c72a124e70cfae59b083f0431f4911c
Farelion <mczownik@gmail.com>
2019-08-19 19:35:51 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,9 @@
   
+  
+  const body = document.body;
+
   const showDialog = () => {
     if ($(".nav-mobile").hasClass("on")){
-      const body = document.body;
       const scrollY = body.style.top;
       body.style.position = '';
       body.style.top = '';
@@ -16,11 +18,21 @@
       $("nav ul").toggleClass('hidden');
       $("nav div").toggleClass('hidden');
       const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-      const body = document.body;
       body.style.position = 'fixed';
       body.style.top = `-${scrollY}`;
     }
   };
+
+  $(".nav-mobile nav ul").on("click", "li", function () {
+    const scrollY = body.style.top;
+    body.style.position = '';
+    body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    $('.menu-toggle').toggleClass("on");
+    $('.nav-mobile').toggleClass("on");
+    $("nav ul").toggleClass('hidden');
+    $("nav div").toggleClass('hidden');
+ });
 
   window.addEventListener('scroll', () => {
     document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
